86db85e1d2514b78f571d252b3b7c164c1597507
Gideon Kamau Maina <gideon.kamau@badili.co.ke>
2018-01-24 12:50:06 +0300

    Change the look of the confirmation template Override the confirm intro class updatign the context remove redundant templates

--- client/html/templates/checkout/confirm/4/intro-body-default.php
+++ client/html/templates/checkout/confirm/4/intro-body-default.php
@@ -7,15 +7,21 @@
  */
 
 $enc = $this->encoder();
-$total = $this->get( 'mydecoratorTotal');
+$total = number_format($this->get( 'mydecoratorTotal'));
 $account_number = $this->get( 'mydecorator_account_number');
 
 ?>
 <?php $this->block()->start( 'checkout/confirm/intro' ); ?>
 
 <div class="checkout-confirm-intro">
-	<p class="note"><?= nl2br( $enc->html( $this->translate( 'client', 'The payment confirmation for your order is still pending.
-You will get an e-mail as soon as the payment is authorized.' ), $enc::TRUST ) ); ?></p>
+	<?php
+		// Create the name of all the products as one string
+		$all_products_names = '';
+		foreach( $this->summaryBasket->getProducts() as $position => $product ) {
+			$all_products_names .= $product->getName().', ';
+		}
+	?>
+	<p class="note"><?= nl2br( $enc->html( $this->translate( 'client', "Your order of $all_products_names Act(s) was placed successfully and a payment of KShs. $total is pending in order to complete the transaction."), $enc::TRUST ) ); ?></p>
 <?= $this->get( 'introBody' ); ?>
 	<?php echo $this->partial( 'checkout/partials/pending-payment.php',['total' => $total, 'account_number' => $account_number ]); ?>
 </div>
