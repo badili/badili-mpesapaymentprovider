46b259def19f16535bbcb71c6b0b513752a5708d
Gideon Kamau Maina <gideon.kamau@badili.co.ke>
2018-02-07 16:58:28 +0300

    remove intro summary basket details

--- client/html/templates/checkout/confirm/4/intro-body-default.php
+++ client/html/templates/checkout/confirm/4/intro-body-default.php
@@ -7,32 +7,12 @@
  */
 
 $enc = $this->encoder();
-$account_number = $this->get( 'mydecorator_account_number');
 
 ?>
 <?php $this->block()->start( 'checkout/confirm/intro' ); ?>
-
 <div class="checkout-confirm-intro">
-	<?php
-		// Create the name of all the products as one string
-		$all_products_names = '';
-		foreach( $this->summaryBasket->getProducts() as $position => $product ) {
-			$all_products_names .= $product->getName().', ';
-		}
-		$currency = $this->summaryBasket->getServices()['payment']->getPrice()->getCurrencyId();
-		$price = $this->summaryBasket->getPrice();
-	    $country = $this->summaryBasket->getAddresses()['payment']->getCountryId();
-		if ($country == "KE") {
-	    	// Do nothing i.e include all the VAT
-	    	$total = $price->getValue() + $price->getCosts() + $price->getTaxValue();
-	    }
-	    else {
-	    	// Remove the tax, only charge the costs and price
-		    $total = $price->getValue() + $price->getCosts();
-	    }
-		$total = number_format($total );
-	?>
-		<p class="note"><?= nl2br( $enc->html( $this->translate( 'client', "Your order of $all_products_names Act(s) was placed successfully and a payment of $currency, $total is pending in order to complete the transaction."), $enc::TRUST ) ); ?></p>
+	<p class="note"><?= nl2br( $enc->html( $this->translate( 'client', 'The payment confirmation for your order is still pending.
+You will get an e-mail as soon as the payment is authorized.' ), $enc::TRUST ) ); ?></p>
 <?= $this->get( 'introBody' ); ?>
 </div>
 <?php $this->block()->stop(); ?>
